1e1d584497e67b084da517ae6e46ff0c718d74de
Vova-Nik <vmaseich@gmail.com>
2020-10-03 19:09:50 +0300

    Parcer O + tests made. Before beginning separate threads. Corrected

---
+++ src/main/java/com/Market.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Market {
+    static String inpFile = "" +
+            "u,9,1,bid\n" +
+            "u,11,5,ask\n" +
+            "q,best_bid\n" +
+            "u,10,2,bid\n" +
+            "q,best_bid\n" +
+            "o,sell,1\n" +
+            "q,size,10\n" +
+            "u,9,3,bid\n" +
+            "u,11,5,ask\n";
+
+    public static void main(String... ignored){
+
+        final long then = System.nanoTime();
+
+        Scanner scanner = new Scanner(inpFile);
+        String toParce;
+
+        DataHolder dataHolder = new DataHolder();
+        Parcer parcer = new Parcer(dataHolder);
+
+        while (scanner.hasNextLine()) {
+            toParce = scanner.nextLine();
+            parcer.parce(toParce);
+        }
+        final long millis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - then);
+        System.out.println("Executed in (ms): " + millis);
+    }
+}
---
+++ src/main/java/com/Parcer.java
@@ -0,0 +1,110 @@
+package com;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Parcer {
+    String file;
+    DataHolder dataHolder;
+
+    public Parcer(DataHolder dataHolder) {
+        this.dataHolder = dataHolder;
+    }
+
+    public int parce(String file) {
+
+        char[] str = file.toCharArray();
+        int toReturn;
+        switch (str[0]) {
+            case 'u':
+                toReturn = parceU(str);
+                break;
+            case 'q':
+                toReturn = parceQ(str);
+                break;
+            case 'o':
+                toReturn = parceO(str);
+                break;
+            default:
+                toReturn = -1;
+        }
+        return toReturn;
+    }
+
+
+    int parceU(char[] str) {
+        int priceLength = 0;
+        int quantLength;
+        int price = 0;
+        int quantity = 0;
+        char operation = ' '; //b == bid, a == ask;
+
+        for (int i = 2; i < dataHolder.U_LENGTH; i++) {
+            if (str[i] == ',') {
+                if (priceLength == 0) {
+                    priceLength = i - 2;
+                    for (int j = 1; j <= priceLength; j++) {
+                        price += pow(j) * (str[i - j] - 0x30);
+                    }
+                   // continue;
+                } else {
+                    quantLength = i - 3 - priceLength;
+                    operation = str[i + 1];
+                    for (int j = 1; j <= quantLength; j++) {
+                        quantity += pow(j) * (str[i - j] - 0x30);
+                    }
+                    break;
+                }
+            }
+        }
+        if (operation == 'a') {
+            dataHolder.updateAsk(price, quantity);
+        } else {
+            dataHolder.updateBid(price, quantity);
+        }
+        return 0;
+    }
+
+    int parceQ(char[] str) {
+        if (str[7] == 'b') {     //best bid
+            return dataHolder.queryBestBid();
+        }
+        if (str[7] == 'a') {     //best ask
+            return dataHolder.queryBestAsk();
+        }
+        int price = 0;
+        for (int i = str.length - 1, j = 1; i >= 7; i--, j++) {
+            price += (str[i] - 0x30) * pow(j);
+        }
+        return dataHolder.querySize(price);
+    }
+
+    int parceO(char[] str) {
+        //        o,buy,<size> - removes <size> shares out of asks, most cheap ones.
+        //        o,sell,<size> - removes <size> shares out of bids, most expensive
+//        int quant = 0;
+//        int price = 0;
+//        if (str[2] == 'b') {
+//            for (int i = str.length - 1, j = 1; i >= 6; i--, j++) {
+//                quantiti += (str[i] - 0x30) * pow(j);
+//            }
+//                       return 0;
+//        }
+//        if (str[2] == 's') {
+//            for (int i = str.length - 1, j = 1; i >= 7; i--, j++) {
+//                quantiti += (str[i] - 0x30) * pow(j);
+//            }
+//            return 0;
+//        }
+
+
+        return -1;
+    }
+
+
+    final int[] exp10 = {1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+
+    int pow(int exp) {
+        return exp10[exp];
+    }
+}
